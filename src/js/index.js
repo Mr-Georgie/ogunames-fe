ed4e0d9456fe627d8da10cf7d84462f029845816
Mr-Georgie <george.isiguzo@yahoo.com>
2023-06-02 13:23:57 +0100

    fixed bugs + navbar is now fixed on scroll

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+var topNav = document.getElementById("sticky");
+var mainNav = document.getElementById("fixed");
+
+window.addEventListener("scroll", function () {
+  // This value is your scroll distance from the top
+  var distance_from_top =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+
+  // The user has scrolled to the tippy top of the page. Set appropriate style.
+  if (distance_from_top === 0) {
+    topNav.classList.remove("hidden");
+    mainNav.classList.remove("fixed", "top-0", "box-shadow");
+  }
+
+  // The user has scrolled down the page.
+  if (distance_from_top > 0) {
+    topNav.classList.add("hidden");
+    mainNav.classList.add("fixed", "top-0", "box-shadow");
+  }
+});
