1263099da066ac54ea5698c90da847c795d0e827
Allenade <allenumunade@gmail.com>
2023-06-23 14:38:43 +0100

    awaiting review

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
-  prefix: '',
+  content: ["./src/**/*.{html,js}"],
+  prefix: "",
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Lato', 'sans-serif'],
+        inter: ["Lato", "sans-serif"],
       },
       boxShadow: {
-        primary: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        primary: "0px 4px 4px rgba(0, 0, 0, 0.25)",
       },
       zIndex: {
-        '-1': '-1',
+        "-1": "-1",
       },
       transformOrigin: {
-        0: '0%',
+        0: "0%",
       },
       colors: {
-        'custom-orange': 'rgb(245,154,23)',
-        'hover-orange': 'rgba(245, 154, 23, 0.9)',
-        'basic-grey': 'rgb(243, 243, 243)',
-        'hr-grey-1': 'rgb(189, 189, 189)',
-        'hr-grey-2': 'rgba(15, 15, 15, 0.3)',
-        'primary-text-color': 'rgb(15, 15, 15)',
-        'light-grey-text': 'rgb(102,102,102)',
+        "custom-orange": "rgb(245,154,23)",
+        "hover-orange": "rgba(245, 154, 23, 0.9)",
+        "basic-grey": "rgb(243, 243, 243)",
+        "hr-grey-1": "rgb(189, 189, 189)",
+        "hr-grey-2": "rgba(15, 15, 15, 0.3)",
+        "primary-text-color": "rgb(15, 15, 15)",
+        "light-grey-text": "rgb(102,102,102)",
       },
     },
   },
@@ -32,6 +32,6 @@ module.exports = {
   },
   plugins: [],
   variants: {
-    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+    borderColor: ["responsive", "hover", "focus", "focus-within"],
   },
-}
+};
